09ee99ebfd4f3dc29109bed8de2dc355c9fc9471
Ernesto Canquiz <caribestic123@gmail.com>
2025-08-11 12:08:09 -0400

    feat: Complement with more useFetch exercises

--- app/pages/UseFetch.vue
+++ app/pages/UseFetch.vue
@@ -1,24 +1,65 @@
 <script setup>
+// https://nuxt.com/docs/4.x/getting-started/data-fetching
 const config = useRuntimeConfig();
+
+const { data, error, execute, status } = await useFetch( () => `${config.public.intApiBase}/hello`, {
+  immediate: false
+});
 
 const { data: intData } = await useFetch(
   () => `${config.public.intApiBase}/hello`
+    //, {
+    //  lazy: false,    /* It's not necessary because by default it's false */
+    //  server: true,   /* It's not necessary because by default it's true */
+    //}
 );
 
-const { data: posts, pending, error } = await useFetch(`${config.public.extApiBase}/posts`, {
-  lazy: true,       // Carga después de montar el componente
-  server: false,    // Solo ejecuta en cliente
-  //pick: ['userId', 'id', 'title', 'body'],  // Solo trae campos específicos
+const { data: post, pending: postPending, error: postError } = await useFetch(`${config.public.extApiBase}/posts/1`, {
+  lazy: false,
+  server: false,
+  pick: [ // Only bring specific fields
+    // 'userId',
+    'id',
+    'title',
+    // 'body'
+  ],  
+});
+
+const { data: posts, pending: postsPending, error: postsError } = await useFetch(`${config.public.extApiBase}/posts`, {
+  lazy: true,       // Load after mounting the component
+  server: false,    // Only runs on client 
+  transform: (posts) => {
+    return posts.map(post => ({
+      // userId: post.userId,
+      id: post.id,
+      title: post.title,
+      // body: post.body
+    }))
+  }
 });
 </script>
 
 <template>
-  <div>{{ intData.message  }}</div>
-  <div v-if="pending">Cargando...</div>
-  <div v-else-if="error">{{ error.message }}</div>
-  <ul v-else>
-    <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+
+  <div v-if="status === 'idle'">
+    <button @click="execute">Get data</button>
+  </div>
+  <div v-else-if="status === 'pending'">
+    Loading greeting...
+  </div>
+  <div v-else>
+    {{ data }}
+  </div>
+
+  <div>{{ intData.message  }}</div> <!-- v-if="intData" It is not necessary because it renders on the server -->
+
+  <div v-if="postPending">Cargando Post...</div>
+  <div v-else-if="postError">{{ postError.message }}</div>
+  <div v-else-if="post">{{ post.title }}</div> <!-- v-if="post" It is not necessary because it renders on the server -->
+
+  <div v-if="postsPending">Cargando Posts...</div>
+  <div v-else-if="postsError">{{ postsError.message }}</div>
+  <ul v-else-if="posts">
+    <li v-for="p in posts" :key="p.id">{{ p.title }}</li>
   </ul>
-
 </template>
-
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/usefetch">useFetch</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
